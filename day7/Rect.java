429d6720591177f264234faaba163f8a341748a1
Miny1110 <wjdalswjd453@naver.com>
2022-06-07 18:49:21 +0900

    Create Rect.java

---
+++ day7/Rect.java
@@ -0,0 +1,71 @@
+package com.day7;
+
+import java.util.Scanner;
+
+public class Rect {
+
+	//전역변수, instance 변수
+	int w, h;
+	
+	
+	//input이라는 부서. 누구나 들어올 수 있음(public). 뭐 들고올 것 없다.(매개변수 없음). 돌려주는 것 없다(void)
+	public void input() {
+		
+		//클래스를 복사할거야 > new
+		//클래스 복사본 > sc
+		Scanner sc = new Scanner(System.in);
+		
+		//지역변수, 로컬변수
+		//int w, h;
+		
+		
+		System.out.println("가로? ");
+		w = sc.nextInt();
+		
+		System.out.println("세로? ");
+		h = sc.nextInt();
+		
+		//void에서는 return 생략 가능
+		//return;
+	}
+	
+
+	
+	//넓이구하기
+	public int area() {
+		
+		//지역변수
+		int result;
+		
+		//w, h는 input메소드에 있는 지역변수. 그래서 area메소드에서 사용 불가(주석처리 된 것)
+		//그래서 복도로 변수를 빼준다. 그것이 전역변수
+		result = w * h;
+		
+		//메소드를 호출한 곳으로 return값을 돌려준다.
+		return result;
+		
+	}
+	
+	
+	
+	//둘레구하기
+	public int length() {
+		
+		return (w+h)*2;
+		
+	}
+
+	
+	
+	//출력
+	public void print(int a, int l) {
+		
+		System.out.println("가로: " + w);
+		System.out.println("세로: " + h);
+		System.out.println("넓이: " + a);
+		System.out.println("둘레: " + l);
+		
+	}
+	
+	
+}
