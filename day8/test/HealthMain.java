6dad63e52fed9671bd1369836495a5fc8ebdd4a1
Miny1110 <wjdalswjd453@naver.com>
2022-06-08 20:33:10 +0900

    Create HealthMain.java

---
+++ day8/test/HealthMain.java
@@ -0,0 +1,16 @@
+package com.day8.test;
+
+public class HealthMain {
+
+	public static void main(String[] args) {
+
+		HealthData hd = new HealthData();
+		
+		hd.set();
+		hd.input();
+		hd.print();
+		
+		
+	}
+
+}
