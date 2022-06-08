e8c551b8dd7c86a4c88d4fe612ab0d9baab0d986
Miny1110 <wjdalswjd453@naver.com>
2022-06-08 20:33:28 +0900

    Create Test3.java

---
+++ day8/Test3.java
@@ -0,0 +1,25 @@
+package com.day8;
+
+import java.util.Calendar;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+//		MyData ob = new MyData();
+		
+//		ob.print();
+		
+		MyData.getInstance();
+
+		
+		
+//		Calendar cal = new Calendar();
+		Calendar cal = Calendar.getInstance();
+		
+		
+		
+		
+	}
+
+}
