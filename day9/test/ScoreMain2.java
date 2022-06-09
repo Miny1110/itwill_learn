86f873e1f7b2bfeee647ee554b5d3efa7cc34fe0
Miny1110 <wjdalswjd453@naver.com>
2022-06-09 19:18:25 +0900

    Create ScoreMain2.java

---
+++ day9/test/ScoreMain2.java
@@ -0,0 +1,16 @@
+package com.day9.test;
+
+public class ScoreMain2 {
+
+	public static void main(String[] args) {
+		
+		Score2 sc = new Score2();
+		
+		sc.set();
+		sc.input();
+		//sc.grade();
+		sc.print();
+		
+	}
+
+}
