f8f6941f0ff57facaa056a6c435430a709140b95
Miny1110 <wjdalswjd453@naver.com>
2022-06-09 11:07:10 +0900

    Update Test3.java

--- day8/Test3.java
+++ day8/Test3.java
@@ -6,10 +6,11 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		
+		//static멤버라 객체생성이 필요없다.
 //		MyData ob = new MyData();
-		
 //		ob.print();
 		
+		//클래스이름.메소드이름 으로 바로 사용 가능하다.
 		MyData.getInstance();
 
 		
