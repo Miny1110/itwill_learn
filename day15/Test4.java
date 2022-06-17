138844256da93739c24dce2bcd0acd4e75032efb
Miny1110 <wjdalswjd453@naver.com>
2022-06-17 17:29:54 +0900

    Create Test4.java

---
+++ day15/Test4.java
@@ -0,0 +1,42 @@
+package com.day15;
+
+class Box<T> { 
+	
+	private T t;
+	
+	public void set(T t) {
+		this.t = t;
+	}
+	
+	public T get() {
+		return t;
+	}
+	
+	
+	
+}
+
+
+public class Test4 {
+
+	public static void main(String[] args) {
+
+		Box b1 = new Box();
+		
+		b1.set(30); //Object에 Integer가 들어감. upcast
+		Integer i = (Integer)b1.get();
+		System.out.println(i);
+		
+		Box<Integer> b2 = new Box<>();
+		b2.set(20);
+		i = b2.get();
+		System.out.println(i);
+		
+		Box<String> b3 = new Box<String>();
+		b3.set("서울");
+		String s = b3.get();
+		System.out.println(s);
+		
+	}
+
+}
