03555f2836d8302b9d55a2895375fd5bed6f6ecb
Miny1110 <wjdalswjd453@naver.com>
2022-06-18 20:51:53 +0900

    Create Ticket.java

---
+++ day15/test/Ticket.java
@@ -0,0 +1,13 @@
+package com.day15.test;
+
+public interface Ticket {
+
+	public void input();
+	public boolean searchID(String ID);
+	public void print();
+	public void delete();
+	public void update();
+	public void searchCname();
+	public void searchPname();
+	
+}
