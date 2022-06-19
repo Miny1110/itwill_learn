c6ce35ee819a5ab21ed9ce2b7a860764a8a5a6ec
Miny1110 <wjdalswjd453@naver.com>
2022-06-19 12:44:16 +0900

    Create Health.java

---
+++ day15/test/Health.java
@@ -0,0 +1,13 @@
+package com.day15.test;
+
+public interface Health {
+
+	public void input();
+	public boolean searchID(String ID);
+	public void print();
+	public void delete();
+	public void update();
+	public void fidnID();
+	public void findName();
+
+}
