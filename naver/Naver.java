354769b961c99a6d33a922f15df91e3b803505fa
Miny1110 <wjdalswjd453@naver.com>
2022-06-20 18:36:10 +0900

    Create Naver.java

---
+++ naver/Naver.java
@@ -0,0 +1,11 @@
+package com.naver;
+
+public interface Naver {
+
+	public void input() throws Exception;
+	public boolean searchID(String id);
+	public void print();
+	public void findID();
+	
+	
+}
