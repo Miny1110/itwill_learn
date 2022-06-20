bbcce57416e883c0bc56b47ead43b20e1cfd93f4
Miny1110 <wjdalswjd453@naver.com>
2022-06-20 18:35:51 +0900

    Update Test5.java

--- day15/Test5.java
+++ day15/Test5.java
@@ -12,7 +12,7 @@ class Box2<T> {
 		return t;
 	}
 	
-	public <U> void print(U u) {
+	public <U> void print(U u) { //메소드에도 자료형을 만들 수 있다.
 		
 		System.out.println(u);
 		System.out.println("t 클래스: " + t.getClass().getName());
@@ -25,11 +25,12 @@ public class Test5 {
 
 	public static void main(String[] args) {
 
-		Box2<Integer> b= new Box2<Integer>();
+		Box2<Integer> b= new Box2<Integer>(); //T의 자료형은 Interger가 된다.
 		
 		b.set(30);
-		b.print("test");
-		b.print(50);
+		b.print("test"); //U의 자료형은 String이 된다.
+		b.print(50); //U의 자료형은 int가 된다.
+		b.print('a'); //U의 자료형은 char가 된다.
 		
 		
 	}
