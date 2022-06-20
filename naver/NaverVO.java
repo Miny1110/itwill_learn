905eb5bcad03de36fcfde862fa2e3043645e4117
Miny1110 <wjdalswjd453@naver.com>
2022-06-20 18:36:24 +0900

    Create NaverVO.java

---
+++ naver/NaverVO.java
@@ -0,0 +1,67 @@
+package com.naver;
+
+public class NaverVO {
+
+	private String id;
+	private String pw1;
+	private String name;
+	private String gender;
+	private String birth;
+	private String email;
+	private String tel;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw1() {
+		return pw1;
+	}
+	public void setPw1(String pw1) {
+		this.pw1 = pw1;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	@Override
+	public String toString() {
+		String str;
+		str = String.format("%s | %s | %s | %s | %s | %s | %s ", 
+				id, pw1, name, gender, birth, email, tel);
+		return str;
+	}
+	
+}
