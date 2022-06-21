6d55c4a0fb2df6503d9169ca1bce58c3fd9cae2e
Miny1110 <wjdalswjd453@naver.com>
2022-06-21 20:24:16 +0900

    Create Test2.java

---
+++ day17/Test2.java
@@ -0,0 +1,49 @@
+package com.day17;
+
+class MyTread2 implements Runnable{
+
+	private int num;
+	private String name;
+
+	public MyTread2(int num, String name) {
+		this.num = num;
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+
+		int i=0;
+
+		while(i<num) {
+
+			System.out.println(name + i);
+			i++;
+
+			try {
+				Thread.sleep(100);	
+			} catch (Exception e) {
+
+			}
+		}
+	}
+}
+
+
+
+public class Test2{
+
+	public static void main(String[] args) {
+
+		System.out.println("main 시작...");
+
+		Thread t1 = new Thread(new MyTread2(100,"첫번째: "));
+		Thread t2 = new Thread(new MyTread2(200,"두번째: "));
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("main 종료...");
+
+	}
+}
