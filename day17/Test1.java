1a14decbe1b0dde9589bca2465b6a98463002391
Miny1110 <wjdalswjd453@naver.com>
2022-06-21 20:23:57 +0900

    Create Test1.java

---
+++ day17/Test1.java
@@ -0,0 +1,52 @@
+package com.day17;
+
+class MyTread1 extends Thread{
+	
+	private int num;
+	private String name;
+	
+	public MyTread1(int num, String name) {
+		this.num = num;
+		this.name = name;
+	}
+
+	//Thread의 run메소드 오버라이드 필수
+	@Override
+	public void run() {
+
+		int i = 0;
+		
+		while(i<num) {
+			//this.getName: 스레드 이름
+			System.out.println(this.getName() + ":" + name + i);
+			i++;
+			
+			//번갈아가면서 출력되게 만들기 위해 작성
+			try {
+				sleep(100); //1000=1초 -> 100=0.1초
+			} catch (Exception e) {
+
+			}
+		}
+	}
+}
+
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("main 시작...");
+		
+		MyTread1 t1 = new MyTread1(100, "첫번째: ");
+		MyTread1 t2 = new MyTread1(200, "두번째: ");
+
+		t1.start(); //run()메소드 실행
+		t2.start(); //run()메소드 실행
+		
+		System.out.println("main 종료...");
+		
+	}
+
+}
