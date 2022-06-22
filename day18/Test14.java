3809ba52c25087f5c0c9e051b7b961c1510bbbf5
Miny1110 <wjdalswjd453@naver.com>
2022-06-22 20:31:45 +0900

    Create Test14.java

---
+++ day18/Test14.java
@@ -0,0 +1,26 @@
+package com.day18;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.Hashtable;
+import java.util.Iterator;
+
+public class Test14 {
+
+	public static void main(String[] args) throws Exception {
+		
+		FileInputStream fis = new FileInputStream("c:\\doc\\out5.txt");
+		
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		Hashtable<String, String> h = 
+				(Hashtable<String, String>)ois.readObject(); //꺼낸 데이터가 Object라 Hashtable로 형변환이 필요하다.
+		
+		Iterator<String> it = h.keySet().iterator();
+		while(it.hasNext()) {
+			String key = it.next();
+			String value = h.get(key);
+			System.out.println(key + ":" + value);
+		}
+	}
+}
