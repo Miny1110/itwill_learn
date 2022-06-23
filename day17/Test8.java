7fa3877e5c97f846920bf32fc855ee8fd99ccd80
Miny1110 <wjdalswjd453@naver.com>
2022-06-23 20:42:37 +0900

    Update Test8.java

--- day17/Test8.java
+++ day17/Test8.java
@@ -23,6 +23,7 @@ class MyThread8 implements Runnable{
 
 			//동기화 블럭: 하나의 스레드가 들어와서 완벽하게 처리하기 전까지 다른 스레드가 들어오지 못하게 하는 것
 			//돈을 다룰 때 마이너스 통장이 생기지 않도록 하기 위해 사용한다
+			//this: 이 객체
 			synchronized (this) { 
 				
 				if(getBank()>=money_need) {
@@ -55,12 +56,12 @@ public class Test8 {
 
 		Thread t1 = new Thread(ob);
 		Thread t2 = new Thread(ob);
+
 		//Thread t1 = new Thread(new MyThread8());
 		//Thread t2 = new Thread(new MyThread8());
 
 		t1.start();
 		t2.start();
-
 
 	}
 
