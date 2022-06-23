ad464cca4ffc6c7709674a4802a291b92d443266
Miny1110 <wjdalswjd453@naver.com>
2022-06-23 20:42:47 +0900

    Update Test14.java

--- day18/Test14.java
+++ day18/Test14.java
@@ -9,12 +9,13 @@ public class Test14 {
 
 	public static void main(String[] args) throws Exception {
 		
+		//역직렬화
 		FileInputStream fis = new FileInputStream("c:\\doc\\out5.txt");
 		
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		
 		Hashtable<String, String> h = 
-				(Hashtable<String, String>)ois.readObject(); //꺼낸 데이터가 Object라 Hashtable로 형변환이 필요하다.
+				(Hashtable<String, String>)ois.readObject(); //꺼낸 데이터가 Object라 Hashtable로 형변환이 필요하다. downcast
 		
 		Iterator<String> it = h.keySet().iterator();
 		while(it.hasNext()) {
