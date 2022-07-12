b6b35e272505caf995e96440fe5e2911adce18f5
Miny1110 <wjdalswjd453@naver.com>
2022-07-12 17:07:51 +0900

    Create Score.java

---
+++ score6/Score.java
@@ -0,0 +1,94 @@
+package com.score6;
+
+import java.util.Scanner;
+
+//사용자한테 데이터를 받아내는 클래스
+
+public class Score {
+
+	Scanner sc = new Scanner(System.in);
+	ScoreDAO dao = new ScoreDAO();
+	
+	//입력 메소드
+	//main에서 호출할 메소드
+	public void insert() {
+		
+		ScoreDTO dto =new ScoreDTO();
+		
+		System.out.print("학번?");
+		dto.setHak(sc.next());
+
+		System.out.print("이름?");
+		dto.setName(sc.next());
+		
+		System.out.print("국어?");
+		dto.setKor(sc.nextInt());
+		
+		System.out.print("영어?");
+		dto.setEng(sc.nextInt());
+		
+		System.out.print("수학?");
+		dto.setMat(sc.nextInt());
+		
+		//int result로 받지 않고 그냥 dao.insertData(dto);만 써도 된다.
+		int result = dao.insertData(dto);
+		
+		if(result!=0) {
+			System.out.println("추가 성공!");
+		}else {
+			System.out.println("추가 실패!");
+		}
+		
+	}
+	
+	
+	//수정 메소드
+	public void update() {
+		
+		ScoreDTO dto = new ScoreDTO();
+		
+		System.out.println("수정할 학번? ");
+		dto.setHak(sc.next());
+		
+		System.out.print("국어?");
+		dto.setKor(sc.nextInt());
+		
+		System.out.print("영어?");
+		dto.setEng(sc.nextInt());
+		
+		System.out.print("수학?");
+		dto.setMat(sc.nextInt());
+		
+		int result = dao.updateData(dto);
+		
+		if(result!=0) {
+			System.out.println("수정 성공!");
+		}else {
+			System.out.println("수정 실패!");
+		}
+		
+	}
+	
+	
+	//삭제 메소드
+	public void delete() {
+		
+		System.out.println("삭제할 학번? ");
+		String delHak = sc.next();
+		
+		int result = dao.deleteData(delHak);
+		
+		if(result!=0) {
+			System.out.println("삭제 성공!");
+		}else {
+			System.out.println("삭제 실패!");
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+}
